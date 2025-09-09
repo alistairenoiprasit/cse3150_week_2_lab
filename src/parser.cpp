7abf8ad5aabdaab7cc1f85c1ad8b72a7cf26d017
Alistaire Noiprasit <alistaire.noiprasit@gmail.com>
2025-09-09 10:59:20 -0400

    Implement functions for parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,3 +1,23 @@
 //
 // Created by Alistaire Noiprasit on 8/9/2025.
 //
+#include "../include/parser.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace StringUtils;
+void StringUtils::stub() {
+    using namespace std;
+    cout << "I'm a stub FROM parseName" << endl;
+}
+
+void StringUtils::parseName(const std::string& fullName, std::string* firstName,
+    std::string* lastName) {
+    std::stringstream(fullName) >> *firstName >> *lastName;
+}
+
+std::string StringUtils::getUsername(const std::string& email) {
+    using namespace std;
+    return email.substr(0, email.find("@"));
+}
